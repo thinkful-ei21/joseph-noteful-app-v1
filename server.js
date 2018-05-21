9bb4ea8270437895bd32e5dbbeb77a585a142d3b
Joseph Kim <gyuhankim@gmail.com>
2018-05-21 14:32:37 -0400

    initial comit

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 'use strict';
 
+// Load express
+const express = require('express');
+
+const app = express();
+
 // Load array of notes
 const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
+// ADD STATIC SERVER HERE
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
 // INSERT EXPRESS APP CODE HERE...
+
